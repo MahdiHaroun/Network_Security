110cb41464867bc06888e71fdd72c89a32bd62f9
MahdiHaroun <mhd0228222@ju.edu.jo>
2025-09-14 20:48:19 +0300

    feat: update FastAPI app structure and add prediction output templates

--- app.py
+++ app.py
@@ -1,93 +1,122 @@
-import sys
-import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
+from fastapi import FastAPI , File , UploadFile , Request 
+from fastapi.responses import Response 
+from starlette.responses import RedirectResponse 
 
-import certifi
-ca = certifi.where()
+import uvicorn
 
-from dotenv import load_dotenv
-load_dotenv()
-mongo_db_url = os.getenv("MONGODB_URL_KEY")
-print(mongo_db_url)
-import pymongo
-from Network_Security.exception.exception import NetworkSecurityException
-from Network_Security.logging.logger import logging
-from Network_Security.piplines.training_pipeline import TrainingPipeline
 
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, File, UploadFile,Request
-from uvicorn import run as app_run
-from fastapi.responses import Response
-from starlette.responses import RedirectResponse
+
+
+import os , sys 
+import certifi 
+from dotenv import load_dotenv 
+import pymongo 
 import pandas as pd
+from datetime import datetime
 
+
+from Network_Security.exception.exception import NetworkSecurityException 
+from Network_Security.logging.logger import logger 
+from Network_Security.piplines.training_pipeline import TrainingPipeline
 from Network_Security.utils.main_utils.utils import load_object
-
 from Network_Security.utils.ml_utils.model.estimator import NetworkModel
 
 
-client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+ca = certifi.where() 
+load_dotenv() 
+uri = "mongodb+srv://MahdiHaroun:"+os.getenv("MONGODB_PASSWORD")+"@cluster0.vgezfpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+print(uri)
+client = pymongo.MongoClient(uri , tlsCAFile=ca)
+from Network_Security.constants.training_pipeline import DATA_INGESTION_DATABASE_NAME , DATA_INGESTION_COLLECTION_NAME 
+database = client[DATA_INGESTION_DATABASE_NAME] 
+collection = client[DATA_INGESTION_COLLECTION_NAME]
 
-from Network_Security.constants.training_pipeline import DATA_INGESTION_COLLECTION_NAME
-from Network_Security.constants.training_pipeline import DATA_INGESTION_DATABASE_NAME
 
-database = client[DATA_INGESTION_DATABASE_NAME]
-collection = database[DATA_INGESTION_COLLECTION_NAME]
 
-app = FastAPI()
-origins = ["*"]
-
+app = FastAPI() 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory="./templates")
 
-@app.get("/", tags=["authentication"])
+@app.get('/') 
 async def index():
     return RedirectResponse(url="/docs")
 
+
 @app.get("/train")
 async def train_route():
-    try:
-        train_pipeline=TrainingPipeline()
-        train_pipeline.run_pipeline()
-        return Response("Training is successful")
-    except Exception as e:
-        raise NetworkSecurityException(e,sys)
-    
+    try : 
+        train_pipline = TrainingPipeline()
+        train_pipline.run_pipeline()
+        return Response ("Training is Successful")
+    except Exception as e: 
+        raise NetworkSecurityException(e, sys) 
+
+from datetime import datetime
+import os
+
+
+
 @app.post("/predict")
-async def predict_route(request: Request,file: UploadFile = File(...)):
+async def predict_route(request: Request, file: UploadFile = File(...)):
     try:
-        df=pd.read_csv(file.file)
-        #print(df)
-        preprocesor=load_object("final_model/preprocessor.pkl")
-        final_model=load_object("final_model/model.pkl")
-        network_model = NetworkModel(preprocessor=preprocesor,model=final_model)
-        print(df.iloc[0])
-        y_pred = network_model.predict(df)
-        print(y_pred)
-        df['predicted_column'] = y_pred
-        print(df['predicted_column'])
-        df['predicted_column'].replace(-1, 0)
-        return df.to_json()
-        df.to_csv('prediction_output/output.csv')
-        table_html = df.to_html(classes='table table-striped')
-        #print(table_html)
-        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+
+        df = pd.read_csv(file.file)
+
+       
+        preprocessor = load_object("final_model/preprocessor.pkl")
+        final_model = load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocessor, model=final_model)
+
         
+        y_pred = network_model.predict(df)
+        df['predicted_column'] = y_pred
+
+        # Replace -1 with 0
+        df['predicted_column'] = df['predicted_column'].replace(-1, 0)
+
+        
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+        
+        original_name = os.path.splitext(file.filename)[0]  
+        safe_name = original_name.replace(" ", "_")         
+
+        
+        output_dir = "prediction_output"
+        os.makedirs(output_dir, exist_ok=True)
+
+       
+        csv_path = os.path.join(output_dir, f"{safe_name}_{timestamp}.csv")
+        json_path = os.path.join(output_dir, f"{safe_name}_{timestamp}.json")
+
+        
+        df.to_csv(csv_path, index=False)
+        df.to_json(json_path, orient='records', lines=True)
+        
+
+       
+        table_html = df.to_html(classes='table table-striped')
+
+        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+
     except Exception as e:
-            raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e, sys)
+
 
     
-if __name__=="__main__":
-    app_run(app,host="0.0.0.0",port=8000)
 
-# bug_1 
+    
 
-'''
-Perfect! Let me modify the logger.py to create a new log file each time it's called, rather than only when the module is imported.'''
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
